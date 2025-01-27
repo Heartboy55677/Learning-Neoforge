b485b7dac92a8433cacf595071083ccd9da44cf2
Heartboy55677 <rohearttless55677@gmail.com>
2025-01-27 02:40:56 -0600

    Delete src/main directory
    This whole section is now redundant

--- src/main/java/net/heartboy/heartboysfirstdraft/item/custom/ModFoodProperties.java
+++
@@ -1,7 +0,0 @@
-package net.heartboy.heartboysfirstdraft.item.custom;
-
-import net.minecraft.world.food.FoodProperties;
-
-public class ModFoodProperties {
-    public static final FoodProperties RADISH = new FoodProperties.Builder().nutrition(3).saturationModifier(.25f).build();
-}
